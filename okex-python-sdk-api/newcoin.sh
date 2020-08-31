f43238690f08ab931e524f08f77bebb2dafc6373
nkyubin <nkyubin@localhost>
2020-08-30 17:30:59 -0700

    new script for adding new coin, example: bash newcoin.sh coin 1min old-coin

---
+++ okex-python-sdk-api/newcoin.sh
@@ -0,0 +1,14 @@
+TEMP=$3
+COIN=$1
+NEW=$2
+
+set -ex
+
+SUF=$(date "+%Y-%m-%d")
+
+  mkdir -p ok_sub_futureusd_${COIN}_kline_swap_${NEW}
+  cp ok_sub_futureusd_${TEMP}_kline_swap_${NEW}.tit2tat_trade_status ok_sub_futureusd_${COIN}_kline_swap_${NEW}.tit2tat_trade_status
+  touch ok_sub_futureusd_${TEMP}_kline_swap_${NEW}.tit2tat_trade_notify.pid 
+  touch ok_sub_futureusd_${TEMP}_kline_swap_${NEW}.tit2tat_trade_notify.log.${SUF} 
+  touch ok_sub_futureusd_${TEMP}_kline_swap_${NEW}.tit2tat_notify.balance
+
