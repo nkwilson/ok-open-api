618221d15232d8ce1be47c4f4280f67e753809d2
nkyubin <nkyubin@localhost>
2021-04-03 21:59:41 +0800

    fix typo error

--- okex-python-sdk-api/newcoin.sh
+++ okex-python-sdk-api/newcoin.sh
@@ -8,7 +8,7 @@ SUF=$(date "+%Y-%m-%d")
 
   mkdir -p ok_sub_futureusd_${COIN}_kline_swap_${NEW}
   cp ok_sub_futureusd_${TEMP}_kline_swap_${NEW}.tit2tat_trade_status ok_sub_futureusd_${COIN}_kline_swap_${NEW}.tit2tat_trade_status
-  touch ok_sub_futureusd_${TEMP}_kline_swap_${NEW}.tit2tat_trade_notify.pid 
-  touch ok_sub_futureusd_${TEMP}_kline_swap_${NEW}.tit2tat_trade_notify.log.${SUF} 
-  touch ok_sub_futureusd_${TEMP}_kline_swap_${NEW}.tit2tat_notify.balance
+  touch ok_sub_futureusd_${COIN}_kline_swap_${NEW}.tit2tat_trade_notify.pid 
+  touch ok_sub_futureusd_${COIN}_kline_swap_${NEW}.tit2tat_trade_notify.log.${SUF} 
+  touch ok_sub_futureusd_${COIN}_kline_swap_${NEW}.tit2tat_notify.balance
 
