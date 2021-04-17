793dc8e925479a70e8b8771c96a7441c99e9152b
nkyubin <nkyubin@localhost>
2021-04-17 09:47:42 +0800

    make it able to create more coins

--- okex-python-sdk-api/newcoin.sh
+++ okex-python-sdk-api/newcoin.sh
@@ -1,14 +1,18 @@
-TEMP=$3
 COIN=$1
 NEW=$2
+CONTRACT=$3
+MODEL=$4
+MODEL_CONTRACT=${5:-swap}
 
 set -ex
 
+echo 'For example, use fil as model, create new coin: newcoin.sh coin 1min contract fil [contract2]'
+
 SUF=$(date "+%Y-%m-%d")
 
-  mkdir -p ok_sub_futureusd_${COIN}_kline_swap_${NEW}
-  cp ok_sub_futureusd_${TEMP}_kline_swap_${NEW}.tit2tat_trade_status ok_sub_futureusd_${COIN}_kline_swap_${NEW}.tit2tat_trade_status
-  touch ok_sub_futureusd_${COIN}_kline_swap_${NEW}.tit2tat_trade_notify.pid 
-  touch ok_sub_futureusd_${COIN}_kline_swap_${NEW}.tit2tat_trade_notify.log.${SUF} 
-  touch ok_sub_futureusd_${COIN}_kline_swap_${NEW}.tit2tat_notify.balance
+  mkdir -p ok_sub_futureusd_${COIN}_kline_${CONTRACT}_${NEW}
+  cp ok_sub_futureusd_${MODEL}_kline_${MODEL_CONTRACT}_${NEW}.tit2tat_trade_status ok_sub_futureusd_${COIN}_kline_${CONTRACT}_${NEW}.tit2tat_trade_status
+  touch ok_sub_futureusd_${COIN}_kline_${CONTRACT}_${NEW}.tit2tat_trade_notify.pid 
+  touch ok_sub_futureusd_${COIN}_kline_${CONTRACT}_${NEW}.tit2tat_trade_notify.log.${SUF} 
+  touch ok_sub_futureusd_${COIN}_kline_${CONTRACT}_${NEW}.tit2tat_notify.balance
 
